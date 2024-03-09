5799849b2b5f048e311d4dde0d945a0aeb2e9851
jromarllegue <lleguejohnromar02@gmail.com>
2024-03-09 08:59:46 -0800

    Update add_checkin.php

--- add_checkin.php
+++ add_checkin.php
@@ -3,9 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Add Check-in/Check-out</title>
 </head>
 <body>
-    
+    <form action="add_checkin.php" method="POST" autocomplete="off">
+        <select name="guest">
+            <?php
+                include_once("config.php");
+                $sql = "SELECT * FROM tbl_checkin";
+
+                $result = $conn->fetch_query($sql);
+
+                while ($row = $row) {
+                    echo "<option value=". $row["guest_id"]. "></option>";
+                }
+
+            ?>
+        </select>
+    </form>
 </body>
 </html>
