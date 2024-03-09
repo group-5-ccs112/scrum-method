2a261e067f590d1b0f4a0c9385f3ce0f72945ed8
natanauanlucky38 <natanauanlucky38@gmail.com>
2024-03-09 08:10:42 -0800

    Create feedbacklist.php

---
+++ feedbacklist.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel Management</title>
+    <style>
+        body {
+            background-color: #add8e6; 
+            font-family: Arial, sans-serif;
+        }
+        h1 {
+            background-color: #7393b3;
+            color: black;
+            padding: 10px;
+        }
+        table {
+            background-color: #add8e6;
+            color: black;
+            border: 1px solid black;
+            border-collapse: collapse;
+            width: 100%;
+        }
+        th, td {
+            border:1px solid #2c3e50;
+            padding: 10px;
+            text-align: left;
+        }
+        th {
+            background-color: #6f8faf;
+            color: black;
+        }
+        .btn {
+            background-color: #6f8faf;
+            border: none;
+            border-radius: 8px;
+            padding: 8px 16px;
+            color: black;
+            cursor: pointer;
+        }
+        .btn:hover {
+            background-color: #6082b6;
+        }
+        .btn-add {
+            text-align: right;
+        }
+    </style>
+</head>
+<body>
+    <div class="container my-5">
+        <h1>Hotel Management</h1>
+        <div class="btn-add">
+            <button a class="btn">ADD</button>
+        </div>
+        <br>
+        <table>
+            <thead>
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <button a class="btn">UPDATE</button>
+                        <button a class="btn">DELETE</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
