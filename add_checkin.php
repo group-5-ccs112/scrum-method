0bc418556b15e05c99efd99a7c7a5ce8aaced15a
jromarllegue <lleguejohnromar02@gmail.com>
2024-03-09 09:38:44 -0800

    Update add_checkin.php

--- add_checkin.php
+++ add_checkin.php
@@ -4,35 +4,62 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Check-in/Check-out</title>
+    <style>
+    </style>
 </head>
-<body>
+<body></body>
     <form action="add_checkin.php" method="POST" autocomplete="off">
-        <select name="guest">
-            <?php
-                include_once("config.php");
+        <table>
+            <tr>
+                <td>
+                    <label>Guest Name</label>
+                </td>
+                <td>
+                    <select name="guest">
+                        <?php
+                            include_once("config.php");
 
-                $sql = "SELECT * FROM tbl_guest";
-                $result = mysqli_query($conn, $sql);
-                while ($row = mysqli_fetch_array($result)) {
-                    echo "<option value=". $row["guest_id"]. ">". $row["first_name"]. " ". $row["last_name"]."</option>";
-                }
-            ?>
-        </select>
-        <select name="room">
-            <?php
-                include_once("config.php");
+                            $sql = "SELECT * FROM tbl_guest";
+                            $result = mysqli_query($conn, $sql);
+                            while ($row = mysqli_fetch_array($result)) {
+                                echo "<option value=". $row["guest_id"]. ">". $row["first_name"]. " ". $row["last_name"]."</option>";
+                            }
+                        ?>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label>Room Name</label>
+                </td>
+                <td>
+                    <select name="room">
+                        <?php
+                            include_once("config.php");
 
-                $sql = "SELECT * FROM tbl_room";
-                $result = mysqli_query($conn, $sql);
-                while ($row = mysqli_fetch_array($result)) {
-                    if ($row["availability"] = "true") {
-                        echo "<option value=". $row["room_id"]. ">". $row["room_type"]. " ". $row["last_name"]."</option>";
-                    }
-                }
-            ?>
-        </select>        
-        <input type="date" name="date_checkin">
-        <input type="submit" name="submit" value="CHECK IN">
+                            $sql = "SELECT * FROM tbl_room";
+                            $result = mysqli_query($conn, $sql);
+                            while ($row = mysqli_fetch_array($result)) {
+                                if ($row["availability"] = "true") {
+                                    echo "<option value=". $row["room_id"]. ">". $row["room_type"]. " ". $row["price"]."</option>";
+                                }
+                            }
+                        ?>
+                    </select>        
+                </td>
+            </tr>
+            <tr>
+                <td>
+                <td>
+                    <label>Room Name</label>
+                </td>
+                </td>
+                <td>
+                    <input type="date" name="date_checkin">
+                </td>
+            </tr>
+                <input type="submit" name="submit" value="CHECK IN">
+            </table>
     </form>
 </body>
 </html>
